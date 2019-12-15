93b5b150972b78b8cf5cfdad43ed23908f1a0489
Mayur-Manvar <mayurpmanvar@gmail.com>
2019-12-16 04:15:09 +0530

    Adding partial view demo

---
+++ MVCPartialViewDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using MVCPartialViewDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPartialViewDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeEntities employeeEntities = new EmployeeEntities();
+            return View(employeeEntities.tblEmployee2.ToList());
+        }
+    }
+}
